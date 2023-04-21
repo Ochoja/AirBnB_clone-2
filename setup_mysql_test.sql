e41126621caa064945f6b0e2851bee9082cce9af
Daniel Ochoja <danielochoja@yahoo.com>
2023-04-21 02:58:59 +0000

    updated

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -6,6 +6,6 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
 -- Set password for user
 SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
 -- Grant privileges
-GRANT ALL PRIVILEGES ON hbhb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- Grant privileges
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
