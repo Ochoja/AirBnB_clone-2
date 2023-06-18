e49da9ca84cea7161890e84f3b31040030e273ac
Daniel Ochoja <danielochoja@yahoo.com>
2023-06-18 04:10:54 +0000

    updated

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Flask Application"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello():
+    return "HBNB"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python():
+    if text != "is cool":
+        return f"Python {escape(text.replace('_', ' '))}"
+    else:
+        return "Python is cool"
+
+
+if __name__ == __main__:
+    app.run(host="0.0.0.0", port=5000)
