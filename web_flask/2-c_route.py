3f8c7af1224b05f965954bbe278663e28dee5aa2
Daniel Ochoja <danielochoja@yahoo.com>
2023-06-18 03:55:07 +0000

    updated

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,16 +7,20 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/c/<text>")
+@app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     return f"C {escape(text.replace('_', ' '))}"
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def index():
     return "Hello HBNB!"
 
 
-@app.route("/hbnb")
+@app.route("/hbnb", strict_slashes=False)
 def hello():
     return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
