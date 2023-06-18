ede254fb075460e4805e4b3f2a4098b17c64e83d
Daniel Ochoja <danielochoja@yahoo.com>
2023-06-18 03:44:09 +0000

    updated script

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,15 +5,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def index():
     """Print message when / is called"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def home():
     """Print message when /hbnb is called"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
