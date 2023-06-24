389c9075f6eb8018c505e0b5c090272e59d6e550
Daniel Ochoja <danielochoja@yahoo.com>
2023-06-24 15:36:32 +0000

    made code pycodestyle compliant

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -6,27 +6,33 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_awesome(text):
     new_text = text.replace('_', ' ')
     return f'C {escape(new_text)}'
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_page(text='is cool'):
     return f'Python {text.replace("_", " ")}'
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def display_number(n):
     return f'{n} is a number'
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
